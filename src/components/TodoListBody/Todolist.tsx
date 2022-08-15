8ac90032b65dd5cd4c64d1b5081538947c29339d
MSokol <mihasokol1993@gmail.com>
2022-08-15 22:18:00 +0300

    Lesson 1, added new component Todolist, threw in some hardcoded state...

---
+++ src/components/TodoListBody/Todolist.tsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+
+type PropsType = {
+    title: string
+    tasks: Array<TasksType>
+}
+
+type TasksType = {
+    id: number
+    title: string
+    isDone: boolean
+}
+export const Todolist = (props: PropsType) => {
+    const {title, tasks} = props
+    return (
+        <div>
+            <h3>{title}</h3>
+            <div>
+                <input/>
+                <button>+</button>
+            </div>
+            <ul>
+                <li><input type="checkbox" checked={tasks[0].isDone}/> <span>{tasks[0].title}</span></li>
+                <li><input type="checkbox" checked={tasks[1].isDone}/> <span>{tasks[1].title}</span></li>
+                <li><input type="checkbox" checked={tasks[2].isDone}/> <span>{tasks[2].title}</span></li>
+            </ul>
+            <div>
+                <button>All</button>
+                <button>Active</button>
+                <button>Completed</button>
+            </div>
+        </div>
+    );
+}
+
